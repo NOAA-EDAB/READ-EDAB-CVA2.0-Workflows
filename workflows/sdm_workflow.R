6e95d71a7efad9edfc6f6a15ee9ec5362200d841
Katherine Gallagher <katherine.gallagher@noaa.gov>
2026-07-15 09:59:52 -0500

    fix: automatically resample bathy raster to be the same resolution as the produced raw_data raster for masking in get_model_data_wrapper; generate new data frame of variable names for forecasts because temperature has a different name in hindcast vs forecasts
    docs: updating documentation for wrapper function, adding note about how to proceed if user wants to normalize data by different averages and standard deviations

--- workflows/sdm_workflow.R
+++ workflows/sdm_workflow.R
@@ -551,7 +551,7 @@ staticR <- terra::rast('~/ClimateVulnerabilityAssessment2.0/SDMs/Data/staticVari
 bathy <- terra::wrap(staticR$bathy) #this is required because of the way terra holds rasters in memory and how things are distributed in parallel with future_map; the bathy raster gets unwrapped within the wrapper function
 
 ####hindcast
-plan(multisession, workers = 3)
+plan(multisession, workers = 8)
 mom6_results <- future_map(
   1:nrow(var.list), 
   ~get_model_data_wrapper(
@@ -573,12 +573,54 @@ plan(sequential)
 
 
 ###decadal forecast
+forecast.list <- data.frame(
+  Long.Name = c(
+    'Sea Water Potential Temperature at Sea Floor',
+    'Bottom Oxygen',
+    'Sea Water Salinity at Sea Floor',
+    'Bottom Aragonite Solubility',
+    'Sea Surface Temperature',
+    'Sea Surface Salinity',
+    'Surface pH',
+    'Mixed layer depth (delta rho = 0.03)',
+    'Diazotroph new (NO3-based) prim. prod. integral in upper 100m',
+    'Small phyto. new (NO3-based) prim. prod. integral in upper 100m',
+    'Medium phyto. new (NO3-based) prim. prod. integral in upper 100m',
+    'Large phyto. new (NO3-based) prim. prod. integral in upper 100m',
+    'Small zooplankton nitrogen biomass in upper 100m',
+    'Medium zooplankton nitrogen biomass in upper 100m',
+    'Large zooplankton nitrogen biomass in upper 100m',
+    'Water column net primary production vertical integral',
+    'Downward Flux of Particulate Organic Carbon'
+  ),
+  Short.Name = c(
+    'bottomT',
+    'bottomO2',
+    'bottomS',
+   'bottomArg',
+    'surfaceT',
+    'surfaceS',
+    'surfacepH',
+    'MLD',
+    'diazPP',
+    'smallPP',
+    'mediumPP',
+    'largePP',
+    'smallZoo',
+    'mediumZoo',
+    'largeZoo',
+    'intNPP',
+    'POC'
+  )
+)
+
+#parallel version
 plan(multisession, workers = 8)
 mom6_results <- future_map(
-  1:nrow(var.list), 
+  1:nrow(forecast.list), 
   ~get_model_data_wrapper(
-    var_name = var.list$Long.Name[.x],
-    short_name = var.list$Short.Name[.x],
+    var_name = forecast.list$Long.Name[.x],
+    short_name = forecast.list$Short.Name[.x],
     json_url = "https://psl.noaa.gov/cefi_portal/data_index/cefi_data_indexing.Projects.CEFI.regional_mom6.cefi_portal.northwest_atlantic.full_domain.decadal_forecast.json",
     release = 'r20250925',
     init = 'i202501',
@@ -587,12 +629,32 @@ mom6_results <- future_map(
     mask_bathy = T,
     bathy = bathy,
     bathy_range = c(-1000, 0),
-    force_overwrite = F
+    force_overwrite = T
   ),
   .progress = T
 )
 plan(sequential)
 
+#because the forecasts have a lot more data to pull from the servers (300+ timestamps for 10 ensemble members), the servers can get angry and the pulls can fail, especially when you are making a lot of requests at the same time. Since the forecasts aren't necessary until the prediction step, the forecast pulls can happen over a longer period (aka overnight if you're in between steps, etc), so below is the option to run the code in sequence if you want to do that 
+
+#for(x in 1:nrow(forecast.list)){
+ # print(Sys.time())
+#  get_model_data_wrapper(
+ #   var_name = forecast.list$Long.Name[x],
+  #  short_name = forecast.list$Short.Name[x],
+   # json_url = "https://psl.noaa.gov/cefi_portal/data_index/cefi_data_indexing.Projects.CEFI.regional_mom6.cefi_portal.northwest_atlantic.full_domain.decadal_forecast.json",
+    #release = 'r20250925',
+#    init = 'i202501',
+ #   spatial_temporal = FALSE,
+  #  source = "forecast",
+   # mask_bathy = T,
+    #bathy = bathy,
+#    bathy_range = c(-1000, 0),
+ #   force_overwrite = T
+  #)
+#  print(x)
+ # print(Sys.time())
+#}
 ##############################
 
 ##############################
--- wrapper_functions/get_model_data_wrapper.R
+++ wrapper_functions/get_model_data_wrapper.R
@@ -1,16 +1,19 @@
 #' @title Pull MOM6 Hindcast or Forecast Data
 #' @description A wrapper function that pulls, averages, and normalizes the MOM6 hindcast or forecast data and saves the output from each step. This function adds log functionality to help with batch runs and running the encompassing functions in parallel. Note that unlike similar functions, there is no skip functionality since this function is meant to only be run once.
-
+#' 
+#' @details
+#' Note that this wrapper normalizes the data by the produced averages and standard deviations (i.e. the timeseries is normalized by its own average and standard deviation). If you wish to normalize the data by a different average/standard deviation (i.e. you want to normalize a forecast timeseries by a hindcast average/standard deviation), it is recommended to load the desired raw, average, and standard deviation data into your environment and run \code{normalize_model_data} separately.  
+#' 
 #' @param source 'hindcast' or 'forecast' to determine which MOM6 data to pull
 #' @param var_name long variable name to pull. Must match names in the 'cefi_long_name' column provided JSON table
 #' @param short_name a shorthand name for \code{var_name}. Used for saving data
 #' @param json_url URL pointing to JSON table variable lists for desired MOM6 run type and domain
 #' @param release release code. Must match one of the options in the 'cefi_release' column in provided JSON table
 #' @param init initialization code. Must match one of the options in the 'cefi_init_date' column in provided JSON table. For forecast only
-#' @param spatial.temporal TRUE/FALSE to determine method for normalizing. If TRUE, normalization is spatially and temporally explicit. If FALSE, normalization occurs using averages/standard deviations calculated across space and time
+#' @param spatial_temporal TRUE/FALSE to determine method for normalizing. If TRUE, normalization is spatially and temporally explicit. If FALSE, normalization occurs using averages/standard deviations calculated across space and time
 #' @param mask_bathy TRUE/FALSE indicating whether or not to use bathymetry data as a mask for raw data
 #' @param bathy a spatRaster of bathymetry data. Must be same extent as results from pull_mom6_hindcast/forecast
-#' @param bathy_range a vector containing the minimum and maximum desired bathymetry values  
+#' @param bathy_range a vector containing the minimum and maximum desired bathymetry values 
 
 #' @return the output from \code{normalize_model_data} - a list whose length is equal to the number of variables supplied, where each item in the list is a spatRaster of data associated with that variable, with the number of layers equal to the number of time steps available. The function also saves the output from each step - see the package website for necessary directory set up.
 
@@ -99,8 +102,10 @@ get_model_data_wrapper <- function(
     if (inherits(bathy, "PackedSpatRaster")) {
       bathy <- terra::unwrap(bathy)
     }
+    #reproject because there are slight differences in resolution/extent for some reason, especially with the forecasts
+    bathy_aligned <- terra::resample(bathy, raw_data, method = "bilinear")
     
-    raw_data <- terra::ifel(bathy <= bathy_range[1] | bathy > bathy_range[2], NA, raw_data)
+    raw_data <- terra::ifel(bathy_aligned <= bathy_range[1] | bathy_aligned > bathy_range[2], NA, raw_data)
     log_info("{short_name} raw {source} data masked with bathymetry between {bathy_range[1]} and {bathy_range[2]}")
   }
   
@@ -124,9 +129,10 @@ get_model_data_wrapper <- function(
     sd = sd_data,
     spatial_temporal = spatial_temporal
   )
-  
+    
   norm_filename <- paste0('./Data/MOM6/norm_', short_name, '_', source, '_', release, '_', bathy_suffix, init_suffix, suffix, '.tif')
   terra::writeRaster(norm_data, filename = norm_filename, overwrite = TRUE)
+  
   log_info("{short_name} data normalized and saved")
   
   return(norm_data)
