c563dc7f47d25a9ba51203a31d2e911c7a11964c
KGallagher7 <katherine.gallagher@noaa.gov>
2026-08-11 14:10:57 -0400

    feature: update logging functions and arguments to pull correct datasets to match other wrapper functions. add switch to use stock shp files if they exist.

--- wrapper_functions/variable_exposures_wrapper.R
+++ wrapper_functions/variable_exposures_wrapper.R
@@ -2,7 +2,9 @@
 #' @description A wrapper function for \code{make_variable_exposure} that handles object loading, and generating mean SDMs from timeseries, and produces both maps and timeseries
 #'
 #' @param spp species name. Used to pull correct data and save outputs in species-specific folders.
-#' @param ens_name name of ensemble species distribution model file to pull
+#' @param spatial_temporal TRUE/FALSE to determine method for normalizing. Helps pull correct ensemble model associated with the MOM6 data with the same name
+#' @param mask_bathy TRUE/FALSE indicating whether or not bathymetry data was used as a mask for raw data before normalization. Helps pull correct ensemble model associated with the MOM6 data with the same name
+#' @param rm_corr TRUE/FALSE indicating whether or not correlated environmental covariates were removed. Helps to pull correct ensemble model
 #' @param sdm_threshold value between 0 and 1. Will remove values lower than this threshold from average ensemble model results to help reduce weird aliasing that can occur in workflow. Defaults to 0.1.
 #' @param present_time,future_time character strings indicating the present and future time series to compare. Example: '1993-2019'. Used to pull correct ranked exposure values and save the data properly
 #'
@@ -10,41 +12,48 @@
 
 variable_exposures_wrapper <- function(
   spp,
-  ens_name,
+  spatial_temporal, mask_bathy, rm_corr,
   sdm_threshold = 0.1,
   present_time,
   future_time
 ) {
-  #open log file
-  sink(file = file.path(getwd(), 'logs', 'variable_averages.log'), append = T)
 
-  # Ensure the sinks are closed when the function exits, regardless of how it exits.
-  on.exit({
-    #sink(type = "message")
-    sink()
-  })
+  # ==========================================================
+  # STEP 0: Set Up
+  # ==========================================================
+  # Set up the logger to output to your specific file
+  log_file <- file.path(getwd(), 'logs', 'ensemble.log')
+  log_appender(appender_file(log_file))
 
-  print(Sys.time())
-  print(spp)
+  log_info("Calculating variable exposures for {spp}")
 
-  #load SDM results and average monthly
-  load(paste0('./', spp, '/output_rasters/', ens_name, '.RData')) #abund
+  #suffixes to help locate correct data
+  suffix <- if(spatial_temporal) "" else "_global"
+  bathy_suffix <- if(mask_bathy) "masked" else ""
+  corr_suffix <- if(rm_corr) "rmcorr" else ""
+
+  # Define standard paths
+  spp_dir        <- file.path(getwd(), spp)
+  predictions_path <- file.path(spp_dir, 'output_rasters', paste0('ENSEMBLE_hindcast_', release, '_', bathy_suffix, suffix, '.tif'))
+
+  # ==========================================================
+  # STEP 1: Load in Data
+  # ==========================================================
+  #model
+  if (!file.exists(predictions_path) | !file.exists(test_name)) {
+    log_error("Ensemble model missing for species: {spp}")
+    stop("Aborting: Ensemble model not found.")
+  }
+  abund <- terra::rast(predictions_path)
 
   #avg ensemble HSM
-  avgHSM <- vector(mode = 'list', length = 12)
-  for (y in 1:12) {
-    mn <- seq(y, length(abund), by = 12)
-    MNS <- raster::stack(abund[mn])
-    avgHSM[[y]] <- raster::calc(MNS, fun = mean, na.rm = T)
-  } #end for
-  avgHSM <- raster::stack(avgHSM)
+  avgHSM <- terra::tapp(abund, rep(1:12, times = terra::nlyr(abund)/12), fun = 'mean')#assuming ensemble is predicted on monthly timesteps and encompases complete years (ie starts in a January and stops in a December), create monthly average data
   names(avgHSM) <- month.abb
 
-  ###remove hsm with less than 0.1 to avoid weird aliasing
-  avgHSM[avgHSM < sdm_threshold] <- NA
+  ###remove hsm with less than threshold to avoid weird aliasing
+  avgHSM<- terra::ifel(avgHSM <= sdm_threshold, NA, avgHSM)
 
-  ###to add: subset avgHSM here by stock area if needed to calculate exposure across time and space appropriately
-
+  #ranked exposure data
   #load in ranked data
   load(paste0(
     './RawExposure/Data/',
@@ -54,6 +63,9 @@ variable_exposures_wrapper <- function(
     '_exposure_ranked.RData'
   )) #expRanked
 
+  # ==========================================================
+  # STEP 2: Calculate Exposures Spatially
+  # ==========================================================
   #map
   mapExp <- make_variable_exposure(
     type = 'map',
@@ -72,11 +84,25 @@ variable_exposures_wrapper <- function(
     )
   )
 
+  log_info('spatial variable exposures for {spp} complete.')
+
+  # ==========================================================
+  # STEP 3: Calculate Exposures Across Time
+  # ==========================================================
+
+  if(file.exists(paste0('./shpfiles/species_stock_areas/', spp, '.shp'))){
+    stocks <- terra::vect(paste0('./shpfiles/species_stock_areas/', spp, '.shp'))
+  } else {
+    stocks <- NULL
+    log_info('No stock shpfiles found for {spp}. Only calculating global exposure timeseries')
+  }
+
   #timeseries
   vecExp <- make_variable_exposure(
     type = 'timeseries',
     ranked_exposure = expRanked,
-    sdm_raster = avgHSM
+    sdm_raster = avgHSM,
+    stock_polys = stocks
   )
   save(
     vecExp,
@@ -89,4 +115,6 @@ variable_exposures_wrapper <- function(
       '/variable_exposure_timeseries.RData'
     )
   )
+
+  log_info('variable exposure timeseries for {spp} complete.')
 }
