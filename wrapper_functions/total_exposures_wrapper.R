71418354e48bac0bbd3312d2cf2b0a38e0da04ec
KGallagher7 <katherine.gallagher@noaa.gov>
2026-08-11 16:12:30 -0400

    feature: add section to compute spatial averages globally and within stock polygons if provided. function now returns data.frame with results.

--- wrapper_functions/total_exposures_wrapper.R
+++ wrapper_functions/total_exposures_wrapper.R
@@ -4,7 +4,7 @@
 #' @param spp species name. Used to pull correct data and save outputs in species-specific folders.
 #' @param present_time,future_time character strings indicating the present and future time series to compare. Example: '1993-2019'. Used to pull correct ranked exposure values and save the data properly
 #'
-#' @return Nothing is returned. The outputs from \code{make_total_exposure(type = 'map')} and \code{make_total_exposure(type = 'timeseries')} are saved in the appropriate folders
+#' @return returns a data.frame containing the spatial averages of total exposure, plus variable exposure across the entire domain, and within stock polygons if shpfiles exists. The outputs from \code{make_total_exposure(type = 'map')} and \code{make_total_exposure(type = 'timeseries')} are saved in the appropriate folders
 
 total_exposures_wrapper <- function(
     spp,
@@ -29,16 +29,15 @@ total_exposures_wrapper <- function(
             'ensemble_weights.rds'))
 
   #variable maps
-  load(
-    file = paste0(
-      file.path(getwd(), spp, 'Data'),
-      '/',
-      present_time,
-      ' vs ',
-      future_time,
-      '/variable_exposure_maps.RData'
+  mapExp <- terra::rast(paste0(
+    file.path(getwd(), spp, 'Data'),
+    '/',
+    present_time,
+    ' vs ',
+    future_time,
+    '/variable_exposure_maps.tif'
     )
-  ) # mapExp - will likely change when naming convention does
+  ) # mapExp - will change when naming convention does
 
   #variable timeseries
   load(
@@ -48,12 +47,12 @@ total_exposures_wrapper <- function(
       present_time,
       ' vs ',
       future_time,
-      '/variable_exposure_timeseries.RData'
+      '/variable_exposure_timeseries.rds'
     )
   ) #vecExp
 
   # ==========================================================
-  # STEP 2: Calculate Exposures Spatially
+  # STEP 2: Calculate Exposures Across Space
   # ==========================================================
 
   #with all variables
@@ -64,16 +63,17 @@ total_exposures_wrapper <- function(
     weights = NA,
     weights_threshold = NA
   )
-  save(
-    mapTot,
-    file = paste0(
+  terra::writeRaster(
+   x = mapTot,
+    filename = paste0(
       file.path(getwd(), spp, 'Data'),
       '/',
       present_time,
       ' vs ',
       future_time,
-      '/total_exposure_map_all_var.RData'
-    )
+      '/total_exposure_map_all_var.tif'
+    ),
+   overwrite = TRUE
   )
 
   #with important variables
@@ -84,22 +84,61 @@ total_exposures_wrapper <- function(
     weights = weights,
     weights_threshold = 0.1
   )
+  terra::writeRaster(
+    x = mapImp,
+    filename = paste0(
+      file.path(getwd(), spp, 'Data'),
+      '/',
+      present_time,
+      ' vs ',
+      future_time,
+      '/total_exposure_map_imp_var.tif'
+    ),
+    overwrite = TRUE
+  )
+
+  log_info('spatial total exposures for {spp} complete.')
+
+  # ==========================================================
+  # STEP 3: Average Exposures Across Space within Stock Regions
+  # ==========================================================
+  #add names and combine rasts
+  names(mapTot) <- 'totalAll'
+  names(mapImp) <- 'totalImp'
+  allRasts <- c(mapTot, mapImp, mapExp)
+
+  #calculate global means
+  globals <- terra::global(allRasts, fun = 'mean', na.rm = T)
+
+  #if stock polygons exist for the species, calculate averages within stocks
+  if(file.exists(paste0('./shpfiles/species_stock_areas/', spp, '.shp'))){
+    stocks <- terra::vect(paste0('./shpfiles/species_stock_areas/', spp, '.shp'))
+    #average within polygons
+  expAvg <- terra::extract(allRasts, stocks, fun = 'mean', na.rm = T)
+
+    #combine
+  expAvgs <- rbind(t(globals), expAvg[,-1])
+  #add stock column
+  expAvg$stock <- c('global', stocks$stock_area)
+
+  } else {
+    log_info('No stock shpfiles found for {spp}. Only calculating global total exposure from maps')
+    expAvg <- t(globals)
+  }
+
   save(
-    mapImp,
+    expAvg,
     file = paste0(
       file.path(getwd(), spp, 'Data'),
       '/',
       present_time,
       ' vs ',
       future_time,
-      '/total_exposure_map_imp_var.RData'
+      '/total_exposure_map_averages.rds'
     )
   )
-
-  log_info('spatial total exposures for {spp} complete.')
-
   # ==========================================================
-  # STEP 3: Calculate Exposures Across Time
+  # STEP 4: Calculate Exposures Across Time
   # ==========================================================
 
   #timeseries - all variables
@@ -143,5 +182,7 @@ total_exposures_wrapper <- function(
   )
 
   log_info('total exposure timeseries for {spp} complete.')
+
+  return(expAvg)
 }
 
