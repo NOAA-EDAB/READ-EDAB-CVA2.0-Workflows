e721305787a8398796073b265f2a03a4e3d65a66
Katherine Gallagher <katherine.gallagher@noaa.gov>
2026-09-22 11:42:22 -0500

    feature: update make_sensitivity_table call with new function name and arguments

--- workflows/sensitivity_workflow.R
+++ workflows/sensitivity_workflow.R
@@ -66,6 +66,8 @@ makereport_sensitivitybarplots(
 )
 
 ##################################
+
+##################################
 ##### FINAL ANALYSIS #############
 ##################################
 
@@ -112,16 +114,41 @@ write.csv(attributeDQ, './Final/data_quality_2_0_final_scores.csv') #save result
 #barplot reports aren't needed for final results, so just make the final tables for species narratives
 
 #make sensitivity tables
+#reload things in if they aren't in already 
+sens <- read.csv('./Final/cva2.0_sensitivity_final_raw_scores.csv') #load in final csv file
+sens <- sens[sens$Attributes != 'Exposure Factor', ] #make sure that only sensitivity is included - expert scores were only used in sensitivity for CVA2.0 but this just makes sure there aren't any empty columns
+sens$Stock.Name <- replace(sens$Stock.Name, sens$Stock.Name == 'Short-finned squid', 'Short finned squid')
+sens$Stock.Name <- replace(sens$Stock.Name, sens$Stock.Name == 'Long-finned squid', 'Long finned squid')
+sens$Stock.Name <- replace(sens$Stock.Name, sens$Stock.Name == "Smooth dogfish shark-Atlantic", 'Smooth dogfish')
+sens <- split_species_stocks(sens, 'Stock.Name')
+sens$Species <- gsub('/', ' ', sens$Species)
 
-makereport_sensitivitytable(
-  species = unique(sens$Stock.Name),
-  species_col = 'Stock.Name',
+sensitivityDF <- read.csv('./Final/sensitivity_2_0_final_scores.csv')
+colnames(sensitivityDF)[1] <- 'Stock.Name'
+sensitivityDF$Stock.Name <- replace(sensitivityDF$Stock.Name, sensitivityDF$Stock.Name == 'Short-finned squid', 'Short finned squid')
+sensitivityDF$Stock.Name <- replace(sensitivityDF$Stock.Name, sensitivityDF$Stock.Name == 'Long-finned squid', 'Long finned squid')
+sensitivityDF$Stock.Name <- replace(sensitivityDF$Stock.Name, sensitivityDF$Stock.Name == "Smooth dogfish shark-Atlantic", 'Smooth dogfish')
+sensitivityDF <- split_species_stocks(sensitivityDF, 'Stock.Name')
+sensitivityDF$Species <- gsub('/', ' ', sensitivityDF$Species)
+
+attributeDQ <- read.csv('./Final/data_quality_2_0_final_scores.csv')
+colnames(attributeDQ)[1] <- 'Stock.Name'
+attributeDQ$Stock.Name <- replace(attributeDQ$Stock.Name, attributeDQ$Stock.Name == 'Short-finned squid', 'Short finned squid')
+attributeDQ$Stock.Name <- replace(attributeDQ$Stock.Name, attributeDQ$Stock.Name == 'Long-finned squid', 'Long finned squid')
+attributeDQ$Stock.Name <- replace(attributeDQ$Stock.Name, attributeDQ$Stock.Name == "Smooth dogfish shark-Atlantic", 'Smooth dogfish')
+attributeDQ <- split_species_stocks(attributeDQ, 'Stock.Name')
+attributeDQ$Species <- gsub('/', ' ', attributeDQ$Species)
+
+make_sensitivity_table(
+  species = unique(sens$Species),
+  species_col = 'Species',
+  stock_col = 'Stock',
   total_sens_col = 'Total.Sensitivity',
   certainty_col = 'Certainty',
-  attribute_names_raw = colnames(sensitivityDF)[2:13],
+  attribute_names_raw = colnames(sensitivityDF)[4:15],
   attribute_names_clean = unique(sens$Attribute.Name),
-  raw.data = sens,
+  raw_data = sens,
   sensitivity = sensitivityDF,
-  data.quality = attributeDQ,
-  table.folder = './Final/Summary_Tables/'
+  data_quality = attributeDQ,
+  table_dir = './Final/Summary_Tables/'
 )
